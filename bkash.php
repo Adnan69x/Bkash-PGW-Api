5c6312e8e1b8fba91569495bca99519b645c7811
ADNAN iTUNE <161193483+Adnan69x@users.noreply.github.com>
2024-05-21 15:05:09 +0600

    Update bkash.php

--- bkash.php
+++ bkash.php
@@ -14,15 +14,20 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 class BkashPGW {
-    private $baseUrl = 'https://tokenized.pay.bka.sh/v1.2.0-beta/tokenized/checkout/';
-    private $appKey = 'YOUR_APP_KEY';
-    private $appSecret = 'YOUR_APP_SECRET';
-    private $username = 'YOUR_USERNAME';
-    private $password = 'YOUR_PASSWORD';
+    private $sandboxBaseUrl = 'https://tokenized.sandbox.bka.sh/v1.2.0-beta/tokenized/checkout/';
+    private $liveBaseUrl = 'https://tokenized.pay.bka.sh/v1.2.0-beta/tokenized/checkout/';
+    private $appKey;
+    private $appSecret;
+    private $username;
+    private $password;
     private $token;
+    private $useSandbox;
+    private $title;
 
     public function __construct() {
         add_action('admin_menu', array($this, 'create_admin_menu'));
+        add_action('admin_init', array($this, 'register_settings'));
+        $this->load_settings();
         $this->grantToken();
     }
 
@@ -37,15 +42,69 @@ class BkashPGW {
         );
     }
 
+    public function register_settings() {
+        register_setting('bkash-pgw-settings-group', 'bkash_enable_sandbox');
+        register_setting('bkash-pgw-settings-group', 'bkash_title');
+        register_setting('bkash-pgw-settings-group', 'bkash_username');
+        register_setting('bkash-pgw-settings-group', 'bkash_password');
+        register_setting('bkash-pgw-settings-group', 'bkash_app_key');
+        register_setting('bkash-pgw-settings-group', 'bkash_app_secret');
+    }
+
+    private function load_settings() {
+        $this->useSandbox = get_option('bkash_enable_sandbox');
+        $this->title = get_option('bkash_title');
+        $this->username = get_option('bkash_username');
+        $this->password = get_option('bkash_password');
+        $this->appKey = get_option('bkash_app_key');
+        $this->appSecret = get_option('bkash_app_secret');
+    }
+
     public function admin_page() {
-        echo '<div class="wrap">';
-        echo '<h1>bKash PGW Settings</h1>';
-        // Add your settings form here
-        echo '</div>';
+        ?>
+        <div class="wrap">
+            <h1><?php echo esc_html($this->title); ?></h1>
+            <form method="post" action="options.php">
+                <?php settings_fields('bkash-pgw-settings-group'); ?>
+                <?php do_settings_sections('bkash-pgw-settings-group'); ?>
+                <table class="form-table">
+                    <tr valign="top">
+                        <th scope="row">Enable Sandbox API</th>
+                        <td><input type="checkbox" name="bkash_enable_sandbox" value="1" <?php checked(1, get_option('bkash_enable_sandbox'), true); ?> /></td>
+                    </tr>
+                    <tr valign="top">
+                        <th scope="row">Title</th>
+                        <td><input type="text" name="bkash_title" value="<?php echo esc_attr(get_option('bkash_title')); ?>" /></td>
+                    </tr>
+                    <tr valign="top">
+                        <th scope="row">Username</th>
+                        <td><input type="text" name="bkash_username" value="<?php echo esc_attr(get_option('bkash_username')); ?>" /></td>
+                    </tr>
+                    <tr valign="top">
+                        <th scope="row">Password</th>
+                        <td><input type="password" name="bkash_password" value="<?php echo esc_attr(get_option('bkash_password')); ?>" /></td>
+                    </tr>
+                    <tr valign="top">
+                        <th scope="row">App Key</th>
+                        <td><input type="text" name="bkash_app_key" value="<?php echo esc_attr(get_option('bkash_app_key')); ?>" /></td>
+                    </tr>
+                    <tr valign="top">
+                        <th scope="row">App Secret</th>
+                        <td><input type="text" name="bkash_app_secret" value="<?php echo esc_attr(get_option('bkash_app_secret')); ?>" /></td>
+                    </tr>
+                </table>
+                <?php submit_button(); ?>
+            </form>
+        </div>
+        <?php
+    }
+
+    private function getBaseUrl() {
+        return $this->useSandbox ? $this->sandboxBaseUrl : $this->liveBaseUrl;
     }
 
     private function request($url, $method = 'POST', $data = null) {
-        $ch = curl_init($this->baseUrl . $url);
+        $ch = curl_init($this->getBaseUrl() . $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
         curl_setopt($ch, CURLOPT_HTTPHEADER, [
@@ -66,7 +125,7 @@ class BkashPGW {
     private function grantToken() {
         $credentials = base64_encode($this->username . ':' . $this->password);
 
-        $ch = curl_init($this->baseUrl . 'token/grant');
+        $ch = curl_init($this->getBaseUrl() . 'token/grant');
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HTTPHEADER, [
             'Content-Type: application/json',
@@ -122,7 +181,7 @@ class BkashPGW {
     public function refreshToken() {
         $credentials = base64_encode($this->username . ':' . $this->password);
 
-        $ch = curl_init($this->baseUrl . 'token/refresh');
+        $ch = curl_init($this->getBaseUrl() . 'token/refresh');
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HTTPHEADER, [
             'Content-Type: application/json',
